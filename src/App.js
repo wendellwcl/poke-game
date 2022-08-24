d357369cca9ad89fa1a80e04bdbcefe0f5c70467
Wendell Corrêa Lima <wendell.wcl19@gmail.com>
2022-08-24 17:08:48 -0300

    testando a API
    teste e leitura da documentação da API: PokeApi

--- src/App.js
+++ src/App.js
@@ -1,4 +1,25 @@
+import { useEffect } from "react";
+
 function App() {
+
+  useEffect(() => {
+
+    function requestPokemon(){
+      fetch('https://pokeapi.co/api/v2/pokemon/ditto/')
+      .then(response => { return response.json()})
+      .then(data => { console.log(data)})
+    };
+    requestPokemon();
+
+    function requestGeneration(){
+      fetch('https://pokeapi.co/api/v2/generation/')
+      .then(response => { return response.json()})
+      .then(data => { console.log(data)})
+    };
+    requestGeneration();
+
+  }, [])
+
   return (
     <div className="App">
       Hello World
