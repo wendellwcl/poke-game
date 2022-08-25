2d4f28f936c3bbfe767b0fb6b06df1962561f59d
Wendell Corrêa Lima <wendell.wcl19@gmail.com>
2022-08-25 19:57:04 -0300

    testes
    testando funcionalidade que será a base para a aplicação; exploração dos dados oferecidos pela api

--- src/App.js
+++ src/App.js
@@ -1,28 +1,35 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
+
+  const [ nome, setNome ] = useState();
+  const [ img, setImg ] = useState();
 
   useEffect(() => {
 
     function requestPokemon(){
       fetch('https://pokeapi.co/api/v2/pokemon/ditto/')
-      .then(response => { return response.json()})
-      .then(data => { console.log(data)})
+      .then(response => { return response.json() })
+      .then(data => { 
+        setNome(data.name); //Nome do Pokemon
+        setImg(data.sprites.other['official-artwork'].front_default); //Imagem do Pokemon
+      });
     };
     requestPokemon();
 
     function requestGeneration(){
       fetch('https://pokeapi.co/api/v2/generation/')
-      .then(response => { return response.json()})
-      .then(data => { console.log(data)})
+      .then(response => { return response.json() })
+      .then(data => {console.log(data)})
     };
     requestGeneration();
 
-  }, [])
+  }, []);
 
   return (
     <div className="App">
-      Hello World
+      <h1>{nome}</h1>
+      <img src={img} alt='alt' />
     </div>
   );
 };
