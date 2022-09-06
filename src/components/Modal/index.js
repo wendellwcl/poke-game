e31c72f5af9ae51fa110cc1125d07dfcaa31e010
Wendell Corrêa Lima <wendell.wcl19@gmail.com>
2022-09-06 13:46:59 -0300

    refactor(components/Modal): Modal adjustments

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,24 +1,21 @@
 import { useEffect, useRef } from 'react';
 import { BsXLg } from 'react-icons/bs';
 
-const Modal = ({ id, children }) => {
+const Modal = ({ id, title, children }) => {
     const modal = useRef();
 
     useEffect(() => {
         const el = modal.current;
         const dismissTriggers = el.querySelectorAll('[data-dismiss]');
         dismissTriggers.forEach((el) => {
-            const dismissTarget = el.getAttribute('data-dismiss');
-            el.addEventListener('click', (e) =>
-                handleCloseModal(e.target, dismissTarget)
-            );
+            el.addEventListener('click', (e) => handleCloseModal(e.target));
         });
     });
 
-    function handleCloseModal(el, target) {
+    function handleCloseModal(el) {
         let targetEl = el.parentNode;
 
-        while (!targetEl.classList.contains(target)) {
+        while (!targetEl.classList.contains('modal')) {
             targetEl = targetEl.parentNode;
         }
 
@@ -27,7 +24,7 @@ const Modal = ({ id, children }) => {
 
     return (
         <div
-            className="modal show"
+            className="modal"
             id={id}
             ref={modal}
             tabIndex="-1"
@@ -37,10 +34,12 @@ const Modal = ({ id, children }) => {
                 data-dismiss="modal"
             ></div>
             <div className="modal-body">
-                <div className="modal-header teste teste2">
+                <div className="modal-header">
+                    {title && <h3>{title}</h3>}
                     <button
                         type="button"
                         data-dismiss="modal"
+                        aria-label="close"
                     >
                         <BsXLg />
                     </button>
