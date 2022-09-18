5eced3adfcf898fc8594d15015839bd8f38a2b32
Wendell Corrêa Lima <wendell.wcl19@gmail.com>
2022-09-17 21:36:27 -0300

    delete: deleting unused files

--- src/helper/ReusableFunctions.js
+++
@@ -1,9 +0,0 @@
-export const fetchJSON = async (url) => {
-    let response;
-
-    await fetch(url)
-        .then((res) => res.json())
-        .then((data) => (response = data));
-
-    return response;
-};
